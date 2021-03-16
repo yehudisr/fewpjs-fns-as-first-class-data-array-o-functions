655c7560146dbff963287071c5edf7c85a1d4f4a
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-03-15 20:46:10 -0400

    commit tree 860c4360d53878669d30a8893ac63d9784f10e15

---
+++ index.js
@@ -0,0 +1,35 @@
+function wakeDog(dogName, dogBreed) {
+    console.log(`Wake ${dogName} the ${dogBreed}`);
+    return `Wake ${dogName} the ${dogBreed}`
+  }
+
+  function leashDog(dogName, dogBreed) {
+    console.log(`Leash ${dogName} the ${dogBreed}`);
+    return `Leash ${dogName} the ${dogBreed}`
+  }
+
+  function walkToPark(dogName, dogBreed) {
+    console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+    return `Walk to the park with ${dogName} the ${dogBreed}`
+  }
+
+  function throwFrisbee(dogName, dogBreed) {
+    console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+    return `Throw the frisbee for ${dogName} the ${dogBreed}`
+  }
+
+  function walkHome(dogName, dogBreed) {
+    console.log(`Walk home with ${dogName} the ${dogBreed}`);
+    return `Walk home with ${dogName} the ${dogBreed}`
+  }
+
+  function unleashDog(dogName, dogBreed) {
+    console.log(`Unleash ${dogName} the ${dogBreed}`);
+    return `Unleash ${dogName} the ${dogBreed}`
+  }
+
+  const routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog]
+
+  function exerciseDog(dog, breed) {
+    return routine.map(fn => fn(dog, breed))
+  }
